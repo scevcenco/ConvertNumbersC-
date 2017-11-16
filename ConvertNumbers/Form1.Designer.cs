dda0365f92c4a59952425c00370e030809407b3b
Anastasia Shevchenko <a.scevcenco@gmail.com>
2017-11-16 19:57:44 +0300

    first commit

---
+++ ConvertNumbers/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ConvertNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Точность дробной части";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(201, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 193);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Результат";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(132, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Десятиричная";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(19, 102);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(131, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Шестнадцатиричная";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Восьмиричная";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Двоичная";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(19, 140);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(19, 67);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Двоичная",
+            "Восьмиричная",
+            "Десятеричная",
+            "Шестнадцатиричная"});
+            this.comboBox1.Location = new System.Drawing.Point(38, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Система счисления";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 278);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Конвертер чисел";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ConvertNumbers/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertNumbers
+{
+    public enum Systems
+    {
+        Двоичная,
+        Восьмиричная,
+        Десятиричная,
+        Шестнадцатиричная
+    }
+    public partial class Form1 : Form
+    {
+        Dictionary<char, double> Pace16 = new Dictionary<char, double>
+        {
+            {'0', 0},
+            {'1', 1},
+            {'2', 2},
+            {'3', 3},
+            {'4', 4},
+            {'5', 5},
+            {'6', 6},
+            {'7', 7},
+            {'8', 8},
+            {'9', 9},
+            {'A', 10},
+            {'B', 11},
+            {'C', 12},
+            {'D', 13},
+            {'E', 14},
+            {'F', 15},
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DataSource = Enum.GetValues(typeof(Systems));   //gets the type, turns enum to array      
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Systems baseFrom;
+            double number, result10;
+            bool flag;
+            int precise;
+            baseFrom = (Systems)comboBox1.SelectedIndex;
+            flag = int.TryParse(textBox2.Text, out precise);
+            if (!flag)
+            {
+                MessageBox.Show("Введите целое число", "Ошибка ввода");
+                return;   
+            }
+            switch (baseFrom)
+            {
+                case Systems.Двоичная:
+                    if (!ConvertToDouble(textBox1.Text, out number))
+                        return;
+                    result10 = ConvertToBase10(number, precise, 2);
+                    textBox6.Text = result10.ToString();
+                    textBox3.Text = textBox1.Text;
+                    textBox4.Text = ConvertToString(ConvertToNewBase(result10, 10, 8, precise));
+                    textBox5.Text = ConvertToString(ConvertToNewBase(result10, 10, 16, precise));
+                    break;
+                case Systems.Восьмиричная:
+                    if (!ConvertToDouble(textBox1.Text, out number))
+                        return;
+                    result10 = ConvertToBase10(number, precise, 8);
+                    textBox6.Text = result10.ToString();
+                    textBox4.Text = textBox1.Text;
+                    textBox3.Text = ConvertToString(ConvertToNewBase(result10, 10, 2, precise));
+                    textBox5.Text = ConvertToString(ConvertToNewBase(result10, 10, 16, precise));
+                    break;
+                case Systems.Десятиричная:
+                    if (!ConvertToDouble(textBox1.Text, out number))
+                        return;
+                    textBox3.Text = ConvertToString(ConvertToNewBase(number, 10, 2, precise));
+                    textBox4.Text = ConvertToString(ConvertToNewBase(number, 10, 8, precise));
+                    textBox6.Text = ConvertToString(ConvertToNewBase(number, 10, 10, precise));
+                    textBox5.Text = ConvertToString(ConvertToNewBase(number, 10, 16, precise));
+                    break;
+                case Systems.Шестнадцатиричная:
+                    result10 = Convert16To10(textBox1.Text, precise);
+                    textBox6.Text = result10.ToString();
+                    textBox5.Text = textBox1.Text;
+                    textBox3.Text = ConvertToString(ConvertToNewBase(result10, 10, 2, precise));
+                    textBox4.Text = ConvertToString(ConvertToNewBase(result10, 10, 8, precise));
+                    break;
+            }
+            
+        }
+        private List<string> ConvertToNewBase (double number, int baseNumber, int convertBaseNumber, int precise)
+        {
+            List<string> result = new List<string>();
+            int integer, integer2;
+            double fractional;
+            integer = Convert.ToInt32(Math.Truncate(number));
+            fractional = number - integer;
+            while (integer>0)
+            {
+                result.Add((integer % convertBaseNumber).ToString());
+                integer /= convertBaseNumber;
+            }
+            result.Reverse();
+            result.Add(".");
+            for (int i=0; i<=precise; i++)
+            {
+                fractional*=convertBaseNumber;
+                integer2 = Convert.ToInt32(Math.Truncate(fractional));
+                result.Add(integer2.ToString());
+                fractional-=integer2;
+            }
+            return result;
+        }
+        private string ConvertToString(List<string> result)
+        {
+            string stringResult = "";
+            foreach (string element in result)
+            {
+                switch(element)
+                {
+                    case "10":
+                        stringResult+="A";
+                        break;
+                    case "11":
+                        stringResult+="B";
+                        break;
+                    case "12":
+                        stringResult+="C";
+                        break;
+                    case "13":
+                        stringResult+="D";
+                        break;
+                    case "14":
+                        stringResult+="E";
+                        break;
+                    case "15":
+                        stringResult+="F";
+                        break;
+                    default: stringResult+=element;
+                        break;
+                }
+            }
+            return stringResult;
+        }
+        private double ConvertToBase10(double input, int precise, int fromBase)
+        {
+            double result = 0;
+            int integer = Convert.ToInt32(Math.Truncate(input));
+            double fractional = input - integer;
+            fractional = Math.Round(fractional, precise);
+            for (int i=0, j=10; i<integer.ToString().Length; i++, j*=10)
+            {
+                result += (integer % j) / (j / 10) * Math.Pow(fromBase, i);
+            }
+            for (int i=-1, j=10; Math.Abs(i)<=precise; i--, j*=10)
+            {
+                result += Math.Truncate(fractional * j) % 10 * Math.Pow(fromBase, i);
+            }
+            return result;
+        }
+        private double Convert16To10(string input, int precise)
+        {
+            string integer, fractional="";
+            integer = input.Split(new char[] { ',', '.' })[0];
+            if (input.Length-1>integer.Length+1)
+            fractional = input.Substring(integer.Length + 1);
+            double result = 0;
+            for (int i = 0; i < integer.Length; i++)
+            {
+                result += Math.Pow(16, i) * Pace16[integer[integer.Length - 1 - i]];
+            }
+            for (int i = -1; Math.Abs(i) <= precise && Math.Abs(i)<=fractional.Length; i--)
+            {
+                result += Math.Pow(16, i) * Pace16[fractional[Math.Abs(i) - 1]];
+            }
+            return result;
+        }
+        private bool ConvertToDouble(string input, out double number)
+        {
+            bool flag = double.TryParse(input, out number);
+            if (!flag)
+            {
+                MessageBox.Show( "Введите число", "Ошибка ввода");
+            }
+            return flag;
+        }
+                  
+    }
+}
